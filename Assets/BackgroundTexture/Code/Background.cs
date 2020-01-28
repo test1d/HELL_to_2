248c65750133b0112b4a1461238b055585eb0405
metalkorshik <nik1998121212@gmail.com>
2020-01-28 22:34:29 +0200

    First Nikita commit

---
+++ Assets/BackgroundTexture/Code/Background.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public List<MonoBehaviour> backgroundList = new List<MonoBehaviour>();
+    public MonoBehaviour player;
+    public Camera mainCamera;
+    public float speed;
+    float halfWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        halfWidth = mainCamera.orthographicSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var playerPos = player.transform.position;
+
+        foreach (var item in backgroundList)
+        {
+            var itemPos = item.transform.position;
+
+            if (playerPos.x + halfWidth > itemPos.x)
+                itemPos = new Vector3(playerPos.x + halfWidth, itemPos.y, 0);
+            else
+                itemPos = new Vector3(itemPos.x - speed, itemPos.y, 0);
+        }
+    }
+}
